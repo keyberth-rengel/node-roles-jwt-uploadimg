7394eca32c8d8a7841469dd174696700bffbe921
keyberth rengel <keyrengel24@gmail.com>
2020-06-20 10:35:06 -0400

    otra prueba

--- server/config/config.js
+++ server/config/config.js
@@ -43,5 +43,5 @@ process.env.URLDB = urlDB;
 //  Google Client ID
 // ============================
 process.env.CLIENT_ID =
-  process.env.CLIENT_ID ||
+  // process.env.CLIENT_ID ||
   "219758474264-vh1bibcphgvbc32km508lubtqkanikf1.apps.googleusercontent.com";
