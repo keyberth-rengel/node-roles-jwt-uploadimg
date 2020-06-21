f9501106161bd7565ddd6a8bd7953414696ad60a
keyberth rengel <keyrengel24@gmail.com>
2020-06-21 16:04:00 -0400

    se saco la carpeta del front

--- react-frontend/src/config/index.js
+++
@@ -1,7 +0,0 @@
-// IP SERVER
-// const IP_SERVER = "http://localhost:5050/api";
-const IP_SERVER = "http://localhost:5050/api";
-
-export default {
-  API_URL: IP_SERVER,
-};
--- react-frontend/src/views/Dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import CircularLoading from "../components/CircularLoading";
-const Dashboard = () => {
-  return <CircularLoading />;
-};
-
-export default Dashboard;
