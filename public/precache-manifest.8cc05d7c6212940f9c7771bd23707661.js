72bbade641178dfa1850693fc361ff197163fed2
keyberth rengel <keyrengel24@gmail.com>
2020-06-25 00:49:06 -0400

    se añadio el build

---
+++ public/precache-manifest.8cc05d7c6212940f9c7771bd23707661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293a94160bc87a9576645dff4510b77d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1abccc302fed416ce24",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c4f06fd4009cd4fea2b8",
+    "url": "/static/js/2.61a145bb.chunk.js"
+  },
+  {
+    "revision": "a1abccc302fed416ce24",
+    "url": "/static/js/main.bdec118e.chunk.js"
+  },
+  {
+    "revision": "03f5dfa609e0bc354180",
+    "url": "/static/js/runtime~main.ec77f0ac.js"
+  },
+  {
+    "revision": "f98da3342837810d2a1e846330bab64e",
+    "url": "/static/media/logo.f98da334.png"
+  },
+  {
+    "revision": "de4be02191b0ac503557433bd5930fd6",
+    "url": "/static/media/profilePic.de4be021.png"
+  }
+]);
